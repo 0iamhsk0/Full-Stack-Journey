60c38a2c9f1cba5b3d1a0a1671cceee1934b1f5e
Hemanth Sai Kumar.P <shumahiroyukiogata@gmail.com>
2025-06-30 14:57:33 +0530

    Updated events

--- HTML-CSS/Events/event.js
+++ HTML-CSS/Events/event.js
@@ -4,6 +4,8 @@ function showMessage(){
 }
 
 const button = document.getElementById("my_button")
-button.onclick = function(){
+button.onclick = function(event){
+    console.log("Event Type: ${event.type}")
+    console.log("Target Element: ${event.target.tagline}")
     alert("External event triggered")
 }
